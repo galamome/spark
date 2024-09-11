051350015ca6d493eddd518231970a20f614ef90
galamome <guillaume.alamome@gmail.com>
2024-09-11 16:50:27 +0200

    Update SparkExampleMain.scala

--- spark_example/src/main/scala/com/example/sparktutorial/SparkExampleMain.scala
+++ spark_example/src/main/scala/com/example/sparktutorial/SparkExampleMain.scala
@@ -1,5 +1,17 @@
 package com.example.sparktutorial
 
+import org.apache.spark.sql.SparkSession
+
+import com.example.sparktutorial.Analysis.calculateAverageTipByPickupLocation
+
 object SparkExampleMain extends App {
-    println("Hello world")
+
+    val spark = createSparkSession("Spark test", isLocal = true)
+
+    val (inputPath, outputPath) = parseArgs(args = args)
+    val data = spark.read.parquet(s"${inputPath}/*.parquet")
+
+    val analysisResult = calculateAverageTipByPickupLocation(data = data)
+
+    analysisResult.write.option("header", "true").csv(outputPath)
 }
