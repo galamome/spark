c09ab7c3913eb2f8231bb9d426136f600e0d8dae
galamome <guillaume.alamome@gmail.com>
2024-09-12 11:16:24 +0200

    Explication de "repartition" vs "coalesce"

--- SparkExampleMain.scala
+++ SparkExampleMain.scala
@@ -1,39 +1,58 @@
 package com.example.sparktutorial
 
 import org.apache.spark.sql.SparkSession
-import org.apache.spark._  // liste des imports
+import org.apache.spark._ // liste des imports
 import org.apache.spark.sql._
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types._
 
 import com.example.sparktutorial.Analysis.calculateAverageTipByPickupLocation
 
-// https://sparktpoint.com/spark-repartition-vs-coalesce/
 object RepartitionVsCoalesceExample extends App {
+  val conf = new SparkConf()
 
-     override def main(args: Array[String]) { // definition de la fonction main
+  val spark = SparkSession.builder()
+    .appName("RepartitionVsCoalesceExample")
+    .master("local[5]")
+    .config(conf)
+    .getOrCreate()
 
-      val conf = new SparkConf()
+  val df = spark.range(0, 20)
 
-      val spark = SparkSession.builder()
-         .appName("RepartitionVsCoalesceExample")
-         .master("local[5]")
-         .enableHiveSupport()
-         .config(conf)
-         .getOrCreate()
+  println("Nombre initial de partitions : " + df.rdd.getNumPartitions)
 
-      import spark.implicits._   // fonctions utilitaires supplementaires
+  // Appelle une fonction d'affichage des partitions et de leur contenu
+  val dfPartitionsMappees = df.rdd.mapPartitionsWithIndex(printDataInPartition)
+  // Le ".count()" sert uniquement à ce que la fonction d'affichage soit exécutée
+  dfPartitionsMappees.count()
 
-    val df = spark.range(0,20)
-    println("Nombre initial de partitions : " + df.rdd.getNumPartitions)
-    df.rdd.mapPartitionsWithIndex(printDataInPartition).count()
-      spark.stop()
-   }
+  // Force la répartition
+  val df2 = df.repartition(6)
 
-  // Définit une fonction pour montrer le contenu de chaque partition
-  def printDataInPartition(index: Int, iterator: Iterator[java.lang.Long]): Iterator[Unit] = {
+  println("Nombre de partitions après forçage par fonction 'repartition(6)': " + df2.rdd.getNumPartitions)
+  // Appelle une fonction d'affichage des partitions et de leur contenu
+  val df2PartitionsMappees = df2.rdd.mapPartitionsWithIndex(printDataInPartition)
+  // Le ".count()" sert uniquement à ce que la fonction d'affichage soit exécutée
+  df2PartitionsMappees.count()
+
+  // Coalesce
+  val df3 = df.coalesce(2)
+
+  println("Nombre de partitions après 'coalesce(2)': " + df3.rdd.getNumPartitions)
+  // Appelle une fonction d'affichage des partitions et de leur contenu
+  val df3PartitionsMappees = df3.rdd.mapPartitionsWithIndex(printDataInPartition)
+  // Le ".count()" sert uniquement à ce que la fonction d'affichage soit exécutée
+  df3PartitionsMappees.count()
+
+  // Arrête la session Spark
+  spark.stop()
+
+  /**
+   * Définit une fonction pour montrer le contenu de chaque partition
+   */
+  private def printDataInPartition(index: Int, iterator: Iterator[java.lang.Long]): Iterator[Unit] = {
     val partitionData = iterator.toList
-    val i  = index +1
+    val i = index + 1
     println(s"Partition $i: ${partitionData.mkString(", ")}")
     Iterator.empty
   }
